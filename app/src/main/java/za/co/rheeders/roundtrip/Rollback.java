74d1d0af58ff2b01b24e84ae11bd84418e6bd1c4
Stefan Rheeders <Rheeders@Macs-MacBook.local>
2021-06-03 13:04:12 +0200

    Completed

--- app/src/main/java/za/co/rheeders/roundtrip/Rollback.java
+++ app/src/main/java/za/co/rheeders/roundtrip/Rollback.java
@@ -17,12 +17,12 @@ public class Rollback {
         if (MainActivity.switchAlgorithm == 0){
             this.bubbleShrinkRoute = new ArrayList<>(Algorithm.bubbleShrinkRoute);
             this.bubbleShrinkDistance = Algorithm.bubbleShrinkDistance;
+            this.smallestCircle = new Circle(Algorithm.smallestCircle);
         }
         this.enteredRoute = new ArrayList<>(Algorithm.enteredRoute);
         this.parameterDiamondRoute = new ArrayList<>(Algorithm.parameterDiamondRoute);
         this.sortedLatitude = new ArrayList<>(Algorithm.sortedLatitude);
         this.parameterDiamondDistance = Algorithm.parameterDiamondDistance;
         this.shortestDistance = Algorithm.shortestDistance;
-        this.smallestCircle = new Circle(Algorithm.smallestCircle);
     }
 }
